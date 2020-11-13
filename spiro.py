6f4e1f91ea34bdae868d29439e1da12c0673994f
clodoveu <clodoveu@dcc.ufmg.br>
2020-11-13 12:35:09 -0300

    Testing commit

---
+++ spiro.py
@@ -0,0 +1,157 @@
+#
+# Spiro
+# V2 2018-07-23
+# CADJ
+#
+# Controls two stepper motors 28BYJ-48 under driver ULN2003
+#  to rotate with varying speeds. Motors drive a drawing mechanism
+#
+#
+
+import RPi.GPIO as GPIO
+import time
+
+# SETUP
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+#  stepper motor 1
+coilA1pin = 4
+coilA2pin = 17
+coilB1pin = 23
+coilB2pin = 24
+#  stepper motor 2
+coilC1pin = 27
+coilC2pin = 22
+coilD1pin = 5
+coilD2pin = 6
+
+motorDelay = 0.005  # seconds between stepper advancements
+
+# GPIO SETUP
+GPIO.setup(coilA1pin, GPIO.OUT)
+GPIO.setup(coilA2pin, GPIO.OUT)
+GPIO.setup(coilB1pin, GPIO.OUT)
+GPIO.setup(coilB2pin, GPIO.OUT)
+GPIO.setup(coilC1pin, GPIO.OUT)
+GPIO.setup(coilC2pin, GPIO.OUT)
+GPIO.setup(coilD1pin, GPIO.OUT)
+GPIO.setup(coilD2pin, GPIO.OUT)
+
+# sequencing for half-step
+stepCount = 8
+seq = []
+seq.append([1, 0, 0, 0])
+seq.append([1, 1, 0, 0])
+seq.append([0, 1, 0, 0])
+seq.append([0, 1, 1, 0])
+seq.append([0, 0, 1, 0])
+seq.append([0, 0, 1, 1])
+seq.append([0, 0, 0, 1])
+seq.append([1, 0, 0, 1])
+STEPA = 0  # global to indicate next step in sequence
+STEPB = 0
+
+
+def cleanup():
+    GPIO.output(coilA1pin, False)
+    GPIO.output(coilA2pin, False)
+    GPIO.output(coilB1pin, False)
+    GPIO.output(coilB2pin, False)
+    GPIO.output(coilC1pin, False)
+    GPIO.output(coilC2pin, False)
+    GPIO.output(coilD1pin, False)
+    GPIO.output(coilD2pin, False)
+    GPIO.output(laserPin, False)
+
+
+def setStepX(s):
+    GPIO.output(coilA1pin, seq[s][0])
+    GPIO.output(coilA2pin, seq[s][1])
+    GPIO.output(coilB1pin, seq[s][2])
+    GPIO.output(coilB2pin, seq[s][3])
+
+
+def setStepY(s):
+    GPIO.output(coilC1pin, seq[s][0])
+    GPIO.output(coilC2pin, seq[s][1])
+    GPIO.output(coilD1pin, seq[s][2])
+    GPIO.output(coilD2pin, seq[s][3])
+
+
+def forwardA():
+    global STEPA
+    setStepA(STEPA)
+    STEPA += 1
+    if STEPA == stepCount:
+        STEPA = 0
+    time.sleep(motorDelay)
+
+
+def backwardA():
+    global STEPA
+    STEPA -= 1
+    if STEPA < 0:
+        STEPA = stepCount - 1
+    setStepA(STEPA)
+    time.sleep(motorDelay)
+
+
+def forwardB():
+    global STEPB
+    setStepB(STEPB)
+    STEPB += 1
+    if STEPB == stepCount:
+        STEPB = 0
+    time.sleep(motorDelay)
+
+
+def backwardB():
+    global STEPB
+    STEPB -= 1
+    if STEPB < 0:
+        STEPB = stepCount - 1
+    setStepB(STEPB)
+    time.sleep(motorDelay)
+
+
+def rotateAfw(steps):
+    for i in range(0, steps):
+        forwardA()
+
+
+def rotateAbw(steps):
+    for i in range(0, steps):
+        backwardA()
+
+
+def rotateBfw(steps):
+    for i in range(0, steps):
+        forwardB()
+
+
+def rotateBbw(steps):
+    for i in range(0, steps):
+        backwardB()
+
+
+def main():
+    # test section while main() is not written
+    while True:
+        steps = input("How many steps forward in X? ")
+        if int(steps) == 0:
+            break
+        forwardX(int(steps))
+        steps = input("How many steps backwards in X? ")
+        backwardX(int(steps))
+        steps = input("How many steps forward in Y? ")
+        forwardY(int(steps))
+        steps = input("How many steps backwards in Y? ")
+        backwardY(int(steps))
+
+    cleanup()
+    GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    main()
