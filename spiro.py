c800980fdd833fda8da5c9380f3708d58a3df247
clodoveu <clodoveu@dcc.ufmg.br>
2021-05-03 17:13:15 -0300

    Three motors

--- spiro.py
+++ spiro.py
@@ -43,8 +43,8 @@ status = RUN
 
 #  motorDelayA = 0.002  # seconds between stepper advancements
 #  motorDelayB = 0.005  # seconds between stepper advancements
-motorSpeedA = 120  # 1/speed = delay in seconds between stepper advancements
-motorSpeedB = -650  # use negative values for backwards rotation
+motorSpeedA = -120  # 1/speed = delay in seconds between stepper advancements
+motorSpeedB = 650  # use negative values for backwards rotation
 motorSpeedC = 400  # use negative values for backwards rotation
 deltaMotorSpeedA = 5
 deltaMotorSpeedB = 5
